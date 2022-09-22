fc196e3c5f3004694c0ccb2bd0ab037c96dd7ff3
Adam Kalmar <adam@kalmar-sk.eu>
2022-09-22 19:29:58 +0200

    cvicenie 1

---
+++ Kalmar1/main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//1.1
+//int main() {
+//    printf("Hello, World!\n");
+//    return 0;
+//}
+
+//1.2
+//int main (){
+//    char znak[1];
+//    printf("zadaj znak: ");
+//    scanf("%s", znak);
+//    printf("%s", znak);
+//    return 0;
+//}
+
+//1.3
+//int main(){
+//    int num1, num2, sum, sum2;
+//
+//    printf("Napiste dve cele cisla: ");
+//
+//    scanf("%d %d", &num1, &num2);
+//
+//    sum = num1 + num2;
+//    sum2 = num1 * num2;
+//
+//    printf("Sucet nacitanych cisel je: %d \n",sum);
+//    printf("Sucin nacitanych cisel je: %d \n",sum2);
+//    return 0;
+//}
+
+//1.4
+//int main(){
+//    int num, sum;
+//
+//    printf("Napiste cele cislo: ");
+//
+//    scanf("%d", &num);
+//
+//    sum = num * num;
+//
+//    printf("Druha mocnina cisla %d je: %d", num, sum);
+//    return 0;
+//}
+
+//1.5
+//int main(){
+//
+//    double num, sum;
+//
+//    printf("Napiste polomer kruhu: ");
+//
+//    scanf("%lf", &num);
+//
+//    sum = 2 * 3.14 * num;
+//
+//    printf("Obvod kruhu s polomerom %f je: %f", num, sum);
+//
+//    return 0;
+//}
+
+//1.6
+//int main(){
+//
+//    printf("\t\\*/Toto je \"backslash\": '\\'\\*/\n");
+//
+//    return 0;
+//}
+
+//1.7
+//int main(){
+//    double num;
+//    printf("Zadaj lubovolne cislo: ");
+//
+//    scanf("%lf", &num);
+//
+//    printf("Cena bez dane:%lf ", num);
+//    printf("Predajna cena s danou 20%% %lf", num *1.2);
+//}
+
+//1.8
+//int main(){
+//    double num1, num2, num3, sum;
+//
+//    printf("Napiste tri cele cisla: ");
+//
+//    scanf("%lf %lf %lf", &num1, &num2, &num3);
+//
+//    sum = num1 * num2 * num3;
+//
+//    printf("Objem: %.3f \n",sum);
+//    return 0;
+//}
+
+//1.9
+//int main() {
+//    printf("Hello, World!\n");
+//    return 0;
+//}
--- test/main.c
+++ test/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
+/*----------------------------------------------------------*/
+/*     Premenne      */
+
+int main (){
+    //deklaracia premennych
+    int i, j;
+    char c;
+    float f;
+    double x;
+
+    //inicializacia premennych
+    i = 10;
+    j = 15;
+    c = 'b';
+    f = 3.1415926535897932384;
+    x = 3.1415926535897932384;
+
+    //vypis viac premennych v jednom printf aj text
+    printf("i = %d j = %d\n", i, j);
+    printf("i+j = %d\n", (i + j));
+    printf("c = %c | %d\n", c, c);
+    printf("f = %f, x = %.16lf\n", f, x);
+
+    scanf(" %d %d", &i, &j);
+    printf("i = %d, j = %d\n", i, j);
+
+    printf("Zadajte velke pismeno: \n");
+    scanf(" %c", &c);
+    c = c + 'a' - 'A';
+
+    printf("Male pismeno: %c\n", c);
+
     return 0;
 }
